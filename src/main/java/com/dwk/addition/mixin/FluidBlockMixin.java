95b1ea4068f46146f5fbb16ac1cd69eaf441460a
MimicEnzymes <143409340+MimicEnzymes@users.noreply.github.com>
2026-03-09 22:08:34 +0800

    Update FluidBlockMixin.java

--- src/main/java/com/dwk/addition/mixin/FluidBlockMixin.java
+++ src/main/java/com/dwk/addition/mixin/FluidBlockMixin.java
@@ -15,9 +15,6 @@ public class FluidBlockMixin {
     @Inject(method = "receiveNeighborFluids", at = @At("HEAD"), cancellable = true)
     private void onReceiveNeighborFluids(World world, BlockPos pos, BlockState state, CallbackInfoReturnable<Boolean> cir) {
         if (DwkSettings.preventFluidInteraction) {
-            // 【修正】这里必须返回 true！
-            // 返回 true 代表 "我还是流体，请继续计算我的流动 tick"
-            // 返回 false 会导致游戏以为它变成了石头，从而停止流动
             cir.setReturnValue(true);
         }
     }
